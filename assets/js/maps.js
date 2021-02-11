e47bf581679ebb3ea2836c13d5dc48a30fec90e2
Mathias-SantAnna <67033618+Mathias-SantAnna@users.noreply.github.com>
2021-02-11 02:59:55 +0000

    Added Google Mpa with Markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,7 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: { lat: 46.619261, lng: -33.134766}
+        zoom: 4,
+        center: { lat: 40.2236296, lng: -85.8829316}
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -15,7 +15,7 @@ function initMap() {
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
-            label: labels{i % labels.length}
+            label: labels[i % labels.length]
         });
     });
 
@@ -23,5 +23,4 @@ function initMap() {
         imagePath:
           "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
       });
-};
-
+}
